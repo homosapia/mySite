db9042c67205f462aadb4c17e1364703f2ab64b5
Павел <88533028+homosapia@users.noreply.github.com>
2021-09-09 17:37:00 +0300

    test

---
+++ My_Site — копия/ViewsModel/ListModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace My_Site.ViewsModel
+{
+    public class ListModel
+    {
+        public PsychicNamber PsychicNamber = new PsychicNamber();
+        public UserNamber UserNamber = new UserNamber();
+    }
+}
