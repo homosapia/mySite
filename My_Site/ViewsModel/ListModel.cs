0ff73909f9f1fcab18ad88aef7c894dba9a0fa62
Павел <88533028+homosapia@users.noreply.github.com>
2021-09-09 17:42:10 +0300

    new

---
+++ My_Site/ViewsModel/ListModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace My_Site.ViewsModel
+{
+    public class ListModel
+    {
+        public PsychicVelue PsychicNamber = new PsychicVelue();
+        public UserNamber UserNamber = new UserNamber();
+    }
+}
