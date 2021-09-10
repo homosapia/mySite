9305bd879d1e63c7d26b4f24606d0d42a4420f1a
asilidien <plkmqasz@mail.ru>
2021-09-10 14:06:08 +0700

    get

---
+++ MySite/MySite/Data/Models/test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySite.Data.Models
+{
+    public class test
+    {
+        public string[] context = new string[] { "dfdf", "dfdere", "dfdght" };
+    }
+}
