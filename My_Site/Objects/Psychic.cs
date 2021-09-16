2da4288f8ea3291b416a1b65e0af0d9e4918c7b4
homosapia <plkmqasz@mail.ru>
2021-09-16 15:24:28 +0700

    save

---
+++ My_Site/Objects/Psychic.cs
@@ -0,0 +1,16 @@
+using My_Site.Objects;
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace My_Site
+{
+    public class Psychic
+    {
+        public int NumberGeneration()
+        {
+            Random random = new Random();
+            return random.Next(10,99);
+        }
+    }
+}
